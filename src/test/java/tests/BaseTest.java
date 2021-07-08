bc41a58869cb9952492f4746a662f28c54d40154
Hayk Bakhshyan <hayk_bakhshyan@epam.com>
2021-07-08 20:56:16 +0400

    Adding parallel testing

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,17 @@
 package tests;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import pagemodels.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -14,13 +21,35 @@ public class BaseTest {
     public SearchPage searchPage;
     public CartPage cartPage;
     public MyAccountPage myAccountPage;
+    public String nodeUrl;
 
+    @Parameters({"browserName"})
     @BeforeTest(alwaysRun = true)
-    public void openBrowser() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://automationpractice.com/");
-        driver.manage().window().maximize();
+    public void openBrowser(String browserName) throws MalformedURLException {
+        //  System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        if (browserName.equals("chrome")) {
+            nodeUrl = "http://192.168.224.1:4444/wd/hub";
+            DesiredCapabilities capability = DesiredCapabilities.chrome();
+            capability.setBrowserName("chrome");
+            capability.setPlatform(Platform.WINDOWS);
+
+            driver = new RemoteWebDriver(new URL(nodeUrl), capability);
+            driver.get("http://automationpractice.com/");
+            driver.manage().window().maximize();
+
+        } else if (browserName.equals("firefox")) {
+
+
+            nodeUrl = "http://192.168.224.1:4444/wd/hub";
+            DesiredCapabilities capability = DesiredCapabilities.firefox();
+            capability.setBrowserName("firefox");
+            capability.setPlatform(Platform.WINDOWS);
+
+            driver = new RemoteWebDriver(new URL(nodeUrl), capability);
+            driver.get("http://automationpractice.com/");
+            driver.manage().window().maximize();
+        }
 
         signInAndOutPage = new SignInAndOutPage(driver);
         wishListPage = new WishListPage(driver);
